12e983fcb4a195d0960b82d58c1bcd53171a41dd
connor pearson <34844662+Liconnorli@users.noreply.github.com>
2023-02-02 13:55:50 +0000

    Fetches all player scores

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 import axiosRetry from 'axios-retry';
-import { Score, ScoreCollection } from './types/LeaderboardData';
+import { Score, ScoreCollection, ScoreImprovement } from './types/LeaderboardData';
 
 axiosRetry(Axios, {
     retries: 3,
@@ -14,17 +14,18 @@ export default class BeatLeaderAPI{
         return response.data as unknown;
     }
 
-    public static async fetchPlayerScores(playerID:string):Promise<ScoreCollection>{
-        
+    public static async fetchPlayerScores(playerID:string,page:number){
+        const scorePage = await this.fetchPage(`player/${playerID}/scores?page=${page}`) as ScoreCollection
+        return scorePage
     }
 
-    public static async fetchAllPlayerScores(playerId : string):Promise<ScoreCollection> {
-        const firstPage = await this.fetchPage(`player/${playerId}/scores`) as ScoreCollection;
+    public static async fetchAllPlayerScores(playerId : string):Promise<unknown> {
+        const firstPage = await this.fetchPlayerScores(playerId,1);
         const totalPages = Math.ceil(firstPage.metadata.total/firstPage.metadata.itemsPerPage)
         let scores = firstPage.data
         const promises = [];
         for (let i = 1; i <= totalPages; i++){
-            const promise= await this.fetchPage(`player/${playerId}/scores?page=${i}`).then(scorePage=>{
+            const promise= await this.fetchPlayerScores(playerId,i).then(scorePage=>{
                 scores = scores.concat(scorePage.data)
             })
             promises.push(promise);
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,8 @@
+import  fs from "fs";
 import BeatLeaderAPI from ".";
 
 async function test() {
-    const me = await BeatLeaderAPI.fetchPlayerScores("76561198308893404")
-    console.log(me)
+    const me = await BeatLeaderAPI.fetchAllPlayerScores("76561198308893404") as string
+    fs.writeFileSync('test.json',JSON.stringify(me))
 }
 test()
